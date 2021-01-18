4620a66b4f274301d3f6844c5616301803eb7204
Lucas <lucas.rodrigues@dextra-sw.com>
2021-01-18 09:20:55 -0300

    Initial Commit

---
+++ src/test/java/tennis/TennisGameTest.java
@@ -0,0 +1,15 @@
+package tennis;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TennisGameTest {
+
+    @Test
+    public void testeSeTaDandoCerto() {
+        Assert.assertTrue(true);
+    }
+}
