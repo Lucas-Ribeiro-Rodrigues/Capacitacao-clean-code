fe43e36ea902a5120a5e3a40bb10f53d14f15b2e
Lucas <lucas.rodrigues@dextra-sw.com>
2021-01-19 19:55:57 -0300

    Finishing setup

--- src/main/java/tennis/TennisGame.java
+++ src/main/java/tennis/TennisGame.java
@@ -2,13 +2,13 @@ package tennis;
 
 public class TennisGame {
 
-    private int m_score1 = 0;
+    protected int m_score1 = 0;
 
-    private int m_score2 = 0;
+    protected int m_score2 = 0;
 
-    private String player1Name;
+    protected String player1Name;
 
-    private String player2Name;
+    protected String player2Name;
 
     public TennisGame(String player1Name, String player2Name) {
         this.player1Name = player1Name;
--- src/test/java/tennis/TennisGameTest.java
+++ src/test/java/tennis/TennisGameTest.java
@@ -1,6 +1,7 @@
 package tennis;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -8,8 +9,32 @@ import org.mockito.junit.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class TennisGameTest {
 
+    private static final String PLAYER_1_NAME = "player1Name";
+
+    private static final String PLAYER_2_NAME = "player2Name";
+
+    private static TennisGame tennisGame;
+
+    @BeforeClass
+    public static void setUp() {
+        tennisGame = new TennisGame(PLAYER_1_NAME, PLAYER_2_NAME);
+    }
+
     @Test
-    public void testeSeTaDandoCerto() {
-        Assert.assertTrue(true);
+    public void testePlayer2GanhouPonto() {
+        tennisGame.m_score1 = 0;
+        tennisGame.m_score2 = 0;
+
+        tennisGame.wonPoint(PLAYER_2_NAME);
+
+        Assert.assertEquals(1, tennisGame.m_score2);
+    }
+
+    @Test
+    public void testeDeuce() {
+        tennisGame.m_score1 = 4;
+        tennisGame.m_score2 = 4;
+
+        Assert.assertEquals("Deuce", tennisGame.getScore());
     }
 }
