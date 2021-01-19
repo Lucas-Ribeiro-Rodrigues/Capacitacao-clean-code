eb3b7636c239c5e6f15b85894f1edb6d1389423e
Lucas <lucas.rodrigues@dextra-sw.com>
2021-01-19 20:12:11 -0300

    Add tests coverage

--- src/test/java/tennis/TennisGameTest.java
+++ src/test/java/tennis/TennisGameTest.java
@@ -21,6 +21,16 @@ public class TennisGameTest {
     }
 
     @Test
+    public void testePlayer1GanhouPonto() {
+        tennisGame.m_score1 = 0;
+        tennisGame.m_score2 = 0;
+
+        tennisGame.wonPoint(PLAYER_1_NAME);
+
+        Assert.assertEquals(1, tennisGame.m_score1);
+    }
+
+    @Test
     public void testePlayer2GanhouPonto() {
         tennisGame.m_score1 = 0;
         tennisGame.m_score2 = 0;
@@ -37,4 +47,160 @@ public class TennisGameTest {
 
         Assert.assertEquals("Deuce", tennisGame.getScore());
     }
+
+    @Test
+    public void testeLoveAll() {
+        tennisGame.m_score1 = 0;
+        tennisGame.m_score2 = 0;
+
+        Assert.assertEquals("Love-All", tennisGame.getScore());
+    }
+
+    @Test
+    public void testeFifteenAll() {
+        tennisGame.m_score1 = 1;
+        tennisGame.m_score2 = 1;
+
+        Assert.assertEquals("Fifteen-All", tennisGame.getScore());
+    }
+
+    @Test
+    public void testeThirtyAll() {
+        tennisGame.m_score1 = 2;
+        tennisGame.m_score2 = 2;
+
+        Assert.assertEquals("Thirty-All", tennisGame.getScore());
+    }
+
+    @Test
+    public void testLoveFifteen() {
+        tennisGame.m_score1 = 0;
+        tennisGame.m_score2 = 1;
+
+        Assert.assertEquals("Love-Fifteen", tennisGame.getScore());
+    }
+
+    @Test
+    public void testLoveThirty() {
+        tennisGame.m_score1 = 0;
+        tennisGame.m_score2 = 2;
+
+        Assert.assertEquals("Love-Thirty", tennisGame.getScore());
+    }
+
+    @Test
+    public void testLoveForty() {
+        tennisGame.m_score1 = 0;
+        tennisGame.m_score2 = 3;
+
+        Assert.assertEquals("Love-Forty", tennisGame.getScore());
+    }
+
+    @Test
+    public void testFifteenLove() {
+        tennisGame.m_score1 = 1;
+        tennisGame.m_score2 = 0;
+
+        Assert.assertEquals("Fifteen-Love", tennisGame.getScore());
+    }
+
+    @Test
+    public void testThirtyLove() {
+        tennisGame.m_score1 = 2;
+        tennisGame.m_score2 = 0;
+
+        Assert.assertEquals("Thirty-Love", tennisGame.getScore());
+    }
+
+    @Test
+    public void testFortyLove() {
+        tennisGame.m_score1 = 3;
+        tennisGame.m_score2 = 0;
+
+        Assert.assertEquals("Forty-Love", tennisGame.getScore());
+    }
+
+
+    @Test
+    public void testFifteenThirty() {
+        tennisGame.m_score1 = 1;
+        tennisGame.m_score2 = 2;
+
+        Assert.assertEquals("Fifteen-Thirty", tennisGame.getScore());
+    }
+
+
+    @Test
+    public void testFifteenForty() {
+        tennisGame.m_score1 = 1;
+        tennisGame.m_score2 = 3;
+
+        Assert.assertEquals("Fifteen-Forty", tennisGame.getScore());
+    }
+
+
+    @Test
+    public void testThirtyFifteen() {
+        tennisGame.m_score1 = 2;
+        tennisGame.m_score2 = 1;
+
+        Assert.assertEquals("Thirty-Fifteen", tennisGame.getScore());
+    }
+
+    @Test
+    public void testThirtyForty() {
+        tennisGame.m_score1 = 2;
+        tennisGame.m_score2 = 3;
+
+        Assert.assertEquals("Thirty-Forty", tennisGame.getScore());
+    }
+
+    @Test
+    public void testFortyFifteen() {
+        tennisGame.m_score1 = 3;
+        tennisGame.m_score2 = 1;
+
+        Assert.assertEquals("Forty-Fifteen", tennisGame.getScore());
+    }
+
+    @Test
+    public void testFortyThirty() {
+        tennisGame.m_score1 = 3;
+        tennisGame.m_score2 = 2;
+
+        Assert.assertEquals("Forty-Thirty", tennisGame.getScore());
+    }
+
+    @Test
+    public void testAdvantagePlayer1() {
+        tennisGame.m_score1 = 4;
+        tennisGame.m_score2 = 3;
+
+        Assert.assertEquals(String.format("Advantage %s", PLAYER_1_NAME), tennisGame.getScore());
+    }
+
+    @Test
+    public void testAdvantagePlayer2() {
+        tennisGame.m_score1 = 3;
+        tennisGame.m_score2 = 4;
+
+        Assert.assertEquals(String.format("Advantage %s", PLAYER_2_NAME), tennisGame.getScore());
+    }
+
+    @Test
+    public void testPlayer1Win() {
+        tennisGame.m_score1 = 4;
+        tennisGame.m_score2 = 2;
+
+        Assert.assertEquals(String.format("Win for %s", PLAYER_1_NAME), tennisGame.getScore());
+    }
+
+    @Test
+    public void testPlayer2Win() {
+        tennisGame.m_score1 = 2;
+        tennisGame.m_score2 = 4;
+
+        Assert.assertEquals(String.format("Win for %s", PLAYER_2_NAME), tennisGame.getScore());
+    }
+
 }
